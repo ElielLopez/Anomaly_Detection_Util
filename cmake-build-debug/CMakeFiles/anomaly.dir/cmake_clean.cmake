2ccc2a2d779bc64e6926caa47f7cbc9de471fb02
ElielLopez <elielsw1@gmail.com>
2020-10-30 00:13:58 +0200

    second commit

---
+++ cmake-build-debug/CMakeFiles/anomaly.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anomaly.dir/main.cpp.o"
+  "anomaly"
+  "anomaly.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/anomaly.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

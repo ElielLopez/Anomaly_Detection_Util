d6b9c4f0e3fecfe0f88a7f9af1f2bc778bc57985
ElielLopez <elielsw1@gmail.com>
2020-10-30 12:50:19 +0200

    first train mode submission, grade 86 check 2- error

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -19,8 +19,8 @@ float avg(float* x, int size){
     }
 
     average = sum / size;
-    cout<<sum<<endl;
-    cout<<"check"<<endl;
+//    cout<<sum<<endl;
+//    cout<<"check"<<endl;
 
     return average;
 }
@@ -85,17 +85,71 @@ float pearson(float* x, float* y, int size){
 }
 
 // performs a linear regression and returns the line equation
+// a = COV(x,y) / VAR(x)
+// b = avgY - a * avgX
 Line linear_reg(Point** points, int size){
 
-    return Line(0,0);
+    float a = 0, b = 0;
+    float covarianceXY = 0;
+    float varianceX = 0;
+    float avgY = 0;
+    float avgX = 0;
+    float x[size];
+    float y[size];
+
+    for (int j = 0; j < size; j++) {
+        x[j] = points[j]->x;
+        y[j] = points[j]->y;
+    }
+
+    for(int i = 0; i < size; i++) {
+        avgY += points[i]->y;
+        avgX += points[i]->x;
+    }
+    avgY = avgY / size;
+    avgX = avgX / size;
+
+    covarianceXY = cov(x, y, size);
+    varianceX = var(x, size);
+
+    a = covarianceXY / varianceX;
+    b = avgY - a * avgX;
+
+    return Line(a,b);
 }
 
 // returns the deviation between point p and the line equation of the points
 float dev(Point p,Point** points, int size){
-    return 0;
+    // p = (4, 10)
+    // 10 = a * 4 + b
+    // line = y = a * x + b
+    float deviation = 0;
+
+    deviation = dev(p, linear_reg(points, size));
+
+    return deviation;
 }
 
 // returns the deviation between point p and the line
+// line y = a*x+b
+// 9 = a * 4 + b
 float dev(Point p,Line l){
-    return 0;
+    // (p.x, p.y)=(4, 10)     (4, 9)
+    float deviation = 0;
+    float px = 0;
+    float py = 0;
+    float lineX = 0;
+    float lineY = 0;
+    float a = 0;
+    float b = 0;
+
+    px = p.x;
+    py = p.y;
+    a = l.a;
+    b = l.b;
+    lineY = a * px + b;
+    // | f(x) - y |
+    deviation = fabs(lineY - py);
+
+    return deviation;
 }
--- main.cpp
+++ main.cpp
@@ -13,59 +13,40 @@ int main(){
     const int N=10;
     float x[]={1,2,3,4,5,6,7,8,9,10};
     float y[]={2.1,4.2,6.1,8.1,10.3,12.2,14.4,16.1,18.2,20.3};
-    float xArray[12] = {178,172,186,180,171,163,175,179,175,178,182,173};
+    //float xArray[12] = {178,172,186,180,171,163,175,179,175,178,182,173};
 
-    float averageX;
-    float averageY;
-    averageX = avg(x, N);
-    averageY = avg(y, N);
-    cout<<averageX<<endl;
-    cout<<averageY<<endl;
-    cout<<var(x, N)<<endl;
-    cout<<var(xArray, 12)<<endl;
+//    float averageX;
+//    float averageY;
+//    averageX = avg(x, N);
+//    averageY = avg(y, N);
+//    cout<<averageX<<endl;
+//    cout<<averageY<<endl;
+//    cout<<var(x, N)<<endl;
+//    cout<<var(xArray, 12)<<endl;
 
     //55 / 10 = 5.5--> pow5.5 = 30.25
     //385 / 10 = 38.5
 
-//    Point* ps[N];
-//    for(int i=0;i<N;i++)
-//        ps[i]=new Point(x[i],y[i]);
-//
-//    Line l=linear_reg(ps,N);
-//    Point p(4,8);
-//
-//    float v[]={var(x,N),cov(x,y,N),pearson(x,y,N),l.a,l.b,l.f(4),dev(p,l)};
-//    float e[]={8.25,16.63,0.999,2.015,0.113,8.176,0.176};
-//
-//
-//    for(int i=0;i<7;i++)
-//        if(wrong(v[i],e[i]))
-//            cout<<"error for check "<<i<<" (-14)"<<endl;
-//
-//
-//    for(int i=0;i<N;i++)
-//        delete ps[i];
+    Point* ps[N];
+    for(int i=0;i<N;i++)
+        ps[i]=new Point(x[i],y[i]);
+        // ps = {(1,2.1),(2,4.2),(3,6.1),(4,8.1),(5,10.3),(6,12.2),(7,14.4),(8,16.1),(9,18.2),(10,20.3)}
+
+    Line l=linear_reg(ps,N);
+    Point p(4,8);
+
+    float v[]={var(x,N),cov(x,y,N),pearson(x,y,N),l.a,l.b,l.f(4),dev(p,l)};
+    float e[]={8.25,16.63,0.999,2.015,0.113,8.176,0.176};
+
+
+    for(int i=0;i<7;i++)
+        if(wrong(v[i],e[i]))
+            cout<<"error for check "<<i<<" (-14)"<<endl;
+
+
+    for(int i=0;i<N;i++)
+        delete ps[i];
 
     cout<<"done"<<endl;
     return 0;
 }
-
-//int main() {
-//    //int argc, char** argv)
-//    std::string prompt = "Hello Motherfuckers";
-//    std::cout<< prompt << std::endl;
-//
-//    Line line (2.2,3.3);
-//    //std::cout<<line.f(5.5)<<std::endl;
-//
-//    float variance;
-//    float covariance;
-//    float xArray[12] = {178,172,186,180,171,163,175,179,175,178,182,173};
-//    float yArray[12] = {170,174,188,188,170,170,175,178,175,178,164,163};
-//
-//    variance = var(xArray,12);
-//    std::cout<<variance<<std::endl;
-//    //covariance = cov(xArray,yArray,12);
-//    //std::cout<<"the covariance is:"<<covariance<<std::endl;
-//    return 0;
-//}
